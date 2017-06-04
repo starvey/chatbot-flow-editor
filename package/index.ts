9c801eb4bacded445c78f47bf2362e5cab00a596
Steven <steven.harvey679@gmail.com>
2017-06-04 01:01:42 -0400

    Able to add node

--- package/index.ts
+++ package/index.ts
@@ -1,1 +1,12 @@
-console.info('Package code loading')
+import { Store } from './store'
+import { addNodeType } from './renderers/NodeTypes'
+
+export class Platform {
+  store: Store
+  constructor () {
+    this.store = new Store()
+    this.store.nodeTypeRegistered.attach((slug) => {
+      addNodeType(this.store.nodeTypes[slug])
+    })
+  }
+}
--- user/index.ts
+++ user/index.ts
@@ -1,1 +1,5 @@
-console.info('User code loading')
+import { Platform } from '../package/index'
+import { MessageNode } from './node-types/Message'
+
+const platform: Platform = new Platform()
+platform.store.registerNodeType(new MessageNode())
