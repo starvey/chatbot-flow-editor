cfc80f658ef146dde31ac7e5ecabe358220e2a1a
Steven <steven.harvey679@gmail.com>
2017-06-05 16:53:09 -0400

    Fix missing node

---
+++ user/node-types/WebApi.ts
@@ -0,0 +1,21 @@
+import { FlowNodeType } from '../../package/classes/FlowNodeType'
+import { FlowNode } from '../../package/classes/FlowNode'
+
+export class WebApiNodeType extends FlowNodeType {
+  slug = "web-api-node"
+  name = "Web API"
+  icon = "http://www.shopspringbreak.com/skin/frontend/default/theme706/fonts/general-ui/svg/message31.svg"
+  backgroundColor = 'red'
+  fontColor = 'black'
+  createNewInstance ({x, y}) {
+    return new MessageNode({
+      flowNodeTypeSlug: this.slug,
+      x,
+      y
+    })
+  }
+}
+
+export class MessageNode extends FlowNode {
+  f__message: string
+}
